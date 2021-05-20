126b41e97e4edce12b7e9d61fada2bb2d4cffdd3
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-05-20 19:07:41 +0530

    Added to show total quantity, totalprice

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -2,14 +2,19 @@ import React from 'react';
 import CartItem from './CartItem';
 import './CartItems.css';
 
-function CartItems( { items }) {
+function CartItems( { items, setcartItems }) {
+  const changeItemQuantity = (e, index) => {
+    const newitems = [...items];
+    newitems[index].quantity = Number(e.target.value);
+    setcartItems(newitems);
+  }
     return (
       <div className="cartItems">
         <h1>Shopping Cart</h1>
         <hr />
         <div className="CartItems-items">
         {items.map((item, index)=> 
-          <CartItem key={index} item={item} />
+          <CartItem index={index} key={index} item={item} changeItemQuantity={changeItemQuantity} />
         )}
         </div>
       </div>
