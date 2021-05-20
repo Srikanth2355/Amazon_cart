8ead85aab933cfb22e755bbbdfd2b199d424f2ba
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-05-20 16:50:43 +0530

    changed Number format

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './CartTotal.css';
-
+import NumberFormat from 'react-number-format';
 function CartTotal({ items }) {
     const getTotalPrice = () => {
         let total = 0;
@@ -9,9 +9,18 @@ function CartTotal({ items }) {
         })
         return total;
     }
+    const getTotalQuantity = () => {
+        let totalquantity = 0;
+        items.forEach((item)=> {
+            totalquantity += item.quantity;
+        })
+        return totalquantity;
+    }
     return (
         <div className="cartTotal">
-            <h3>Subtotal({items.length} items):<span className="cartTotal-price">${getTotalPrice()}</span></h3>
+            <h3>Subtotal({getTotalQuantity()} items):<span className="cartTotal-price">
+            <NumberFormat value={getTotalPrice()} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
+            </span></h3>
             <button>Proceed to Checkout</button>
         </div>
     )
