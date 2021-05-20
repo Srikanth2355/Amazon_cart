283df27f25db1bf07401407c2c251f950cfc5fc4
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-05-20 16:09:18 +0530

    Add cart total Html and style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -3,7 +3,10 @@ import './CartTotal.css';
 
 function CartTotal() {
     return (
-        <div className="cartTotal"></div>
+        <div className="cartTotal">
+            <h3>Subtotal(5 items):<span className="cartTotal-price">$2,178.38</span></h3>
+            <button>Proceed to Checkout</button>
+        </div>
     )
 }
 
